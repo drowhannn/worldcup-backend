8ece4357fe05e65fc9a03a4447ebb6bad2b83a27
Rohan Dhimal <drowhann@gmail.com>
2022-11-05 08:39:20 +0545

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(8000);
 }
